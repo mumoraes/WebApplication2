b0aab4aed8f3b8962613c62d05e69b589eb06353
Murilo Moraes <moraesimurilo@gmail.com>
2020-07-25 14:06:28 -0400

    Add project files.

---
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+
+namespace WebApplication2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Time { get; set; }
+        
+        public void OnGet()
+        {
+            Time = DateTime.Now.ToShortTimeString();
+        }
+    }
+}
